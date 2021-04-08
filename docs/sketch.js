068e90010f484a31ad879ddfd5e991b80db38bf3
llight <laneylight@gmail.com>
2021-04-08 09:34:07 -0400

    separate pages, sort options
    Create separate home, about, and explore pages. Explore contains just the interactive part, explanations of visualizations are on the about page.  Create similarity metric. Add drop down to sort randomly or sort by similarity.

--- docs/sketch.js
+++ docs/sketch.js
@@ -17,6 +17,7 @@ var innerHeightSmall;
 var timeSigNum;
 var shapeSize;
 var nLines;
+var spotifyID;
 var keyTypesList=[];
 var tempoList=[];
 var timeSigNumList=[];
@@ -31,6 +32,7 @@ var voiceList=[];
 var woodwindsList=[];
 var stringsList=[];
 var percussionList=[];
+var brassList=[];
 var fileList=[];
 var midiFileList=[];
 var scoreList=[];
@@ -49,9 +51,9 @@ var scoreOpen=0;
 var loc;
 var spotifyOpen=0;
 var imgtemp;
+var toggleRandom=1;
 
 function preload(){
- //jsonurl='https://www.asdesigned.com/6310examples/proxy.php?url=https://raw.githubusercontent.com/LLight/classical-music-viz/main/features.json';
   featureData=loadJSON('features.json');
 
   violin = loadImage('images/violin_solid.svg');
@@ -60,148 +62,90 @@ function preload(){
   keyboard = loadImage('images/keys_solid.svg');
   drum = loadImage('images/drum_solid.svg');
   singer = loadImage('images/singer.svg');
-  families = loadImage('images/families.svg');
-  difficultyEx = loadImage('images/difficulty.png');
 }
 
 function setup() {
-  //createCanvas(800, 800);
-  myCanvas=createCanvas(windowHeight, windowHeight-80);
-  myCanvas.parent('myContainer');
-  background(240);
+//  print('padding=',document.getElementById('navbar').padding);
+  navbarH=document.getElementById('navbar').clientHeight;
+  canvasH = windowHeight - navbarH- 80;
+  canvasW=canvasH;
+  myCanvas=createCanvas(canvasW,canvasH);  //create square canvas, size 80 pct of total window height
+  myCanvas.parent('canvasContainer');
+  yoffset=document.getElementById('exploreContainer').offsetTop; //y coordinate of top of canvas
+  totalW=document.getElementById('exploreContainer').clientWidth;  //total width of screen (canvas plus area on right side)
+
+  textHeight=floor(canvasH/40);
+  textSize(textHeight);
   noLoop();
   rectMode(CENTER);
   nscores = Object.keys(featureData).length;
-  helpButton = createButton('?');
-  helpButton.position(10, 10);
-  helpButton.mouseOver(question);
 
-  playButton = createButton('Play MIDI');
-  playButton.position(3*width/4, height/2);
-  playButton.mousePressed(toggleMIDI);
+  sel=createSelect();
+  sel.position(20,yoffset);
+  sel.option('Show random pieces');
+  sel.option('Show pieces similar to current selection');
+  sel.changed(selectEvent);
 
   viewButton = createButton('Preview score');
-  viewButton.position(3*width/4, height/2-height/8);
+  viewButton.position(3*canvasW/4, yoffset + 3*canvasH/8);
   viewButton.mousePressed(() => showScore());
 
+  playButton = createButton('Play MIDI');
+  playButton.position(3*canvasW/4, yoffset+canvasH/2);
+  playButton.mousePressed(toggleMIDI);
+
   openSpotifyButton=createButton('Listen on Spotify');
-  openSpotifyButton.position(3*width/4,height/2+height/8);
+  openSpotifyButton.position(3*canvasW/4, yoffset + 5*canvasH/8);
   openSpotifyButton.mousePressed(openSpotify);
 
   closeSpotifyButton=createButton("X");
-  closeSpotifyButton.position(myCanvas.width-closeSpotifyButton.height,myCanvas.height-closeSpotifyButton.height);
+  closeSpotifyButton.position(canvasW-closeSpotifyButton.height,yoffset + canvasH - closeSpotifyButton.height);
   closeSpotifyButton.mousePressed(closeSpotify);
 
   closeScoreButton=createButton('Close score');
-  closeScoreButton.position(width-closeScoreButton.width,closeScoreButton.height);
+  closeScoreButton.position(canvasW, yoffset);
   closeScoreButton.mousePressed(closeScore);
 
   downloadButton = createButton('Download Score');
-  downloadButton.position(width-downloadButton.width,downloadButton.height*3);
+  downloadButton.position(canvasW, yoffset + downloadButton.height*2);
   downloadButton.mousePressed(downloadScore);
-  //closeButton = createButton('Close');
-  //closeButton.position(width/6,height/20);
-  //closeButton.mousePressed(closeScore);
 
-  playButton.hide();
-  viewButton.hide();
-  helpButton.hide();
-  openSpotifyButton.hide();
   closeScoreButton.hide();
   downloadButton.hide();
   closeSpotifyButton.hide();
-  //downloadButton.hide();
-  //closeButton.hide();
 }
 
 function draw(){
   noLoop();
+  console.log('toggle random=',toggleRandom);
 
   pieceData(featureData);
 
-  //console.log(featureData);
   [majorSlowColors, minorSlowColors, majorFastColors, minorFastColors] = defineColors();
-
-  textHeight=floor(windowHeight/40);
 
   //intro page
   if (frameCount==1) {
     for (i=0; i < fileList.length; i++){
     scoreList.push('images/' + fileList[i].split(".")[0] + '.png');
-  }
-    print ('nscores=',nscores);
-    background(255);
-    textAlign(CENTER);
-    textSize(textHeight+14);
-    text('Score Visualizations',width/2,height/4);
-    textSize(textHeight+6);
-    //textStyle(ITALIC);
-    text('A visual exploration tool for sheet music',width/2,3*height/4);
-    textSize(textHeight);
-    textStyle(NORMAL);
-    text('Click anywhere to start',width/2,height-textHeight);
-    randomSort();
-    intro1();
-  }
-  //explain representations of mood through color palettes
-  else if (frameCount==2) {
-   push();
-   background(255);
-   textAlign(CENTER);
-   textSize(textHeight);
-   text('Click anywhere to continue',width/2,height-textHeight);
-   textSize(textHeight+12);
-   text('Color palettes',width/2,height/12);
-   pop();
-   intro2();
-   //playButton.hide();
-  }
-  //explain representations of time signature (shape) and note density (number of lines)
-  else if (frameCount==3){
-   push();
-   background(255);
-   textAlign(CENTER,CENTER);
-   textSize(textHeight+6);
-   text('Time Signature',width/2,height/8);
-   text('Note Density',width/2,height/2);
-   textSize(textHeight);
-   text('In 3 or 6',width/3, 3*height/8);
-   text('In 2 or 4', 2*width/3, 3*height/8);
-   text('Fewer notes per second',width/3,5*height/6,width/4,textHeight*3);
-   text('More notes per second',2*width/3,5*height/6,width/4,textHeight*3);
-   text('Click anywhere to continue',width/2,height-textHeight);
-   pop();
-
-   intro3();
+    }
   }
 
-  else if (frameCount==4){
-      push();
-      textSize(textHeight+6);
 
-      image(families,width*.3,height*.1,width*.4,width*.4*families.height/families.width);
-      image(difficultyEx,width*.2,height*.6,width*.5,width*.5*difficultyEx.height/difficultyEx.width);
-      text('Instrument families',width*.5,height*.1);
-      text('Difficulty level',width*.5,height*.6);
-      textSize(textHeight);
-      text('Easy',width*.8,height*.7);
-      text('Hard',width*.8,height*.85);
-      pop();
-      text('Click anywhere to continue',width/2,height-textHeight);
-  }
-  //start the main part of the visualizations
   //display random pieces with one currently selected shown larger in the middle
   //other random pieces are smaller at the top and bottom of the screen
-  else  {
-    background(255);
 
+    viewButton.show();
     playButton.show();
     viewButton.show();
-    helpButton.show();
     openSpotifyButton.show();
 
     if (randomize==1){
-      randomSort();
+      if (toggleRandom==1){
+        randomSort();
+      }
+      else if (toggleRandom==0){
+        sortSimilar();
+      }
       isPlaying=0;
       playbackStarted=0;
       MIDIjs.stop();
@@ -224,7 +168,8 @@ function draw(){
 
     for (let i=0; i<7; i++){
 
-      if (frameCount>5 && i==0){
+      if (frameCount>1 && i==0){
+      //if (i==0){
         randomPiece=newCenterPiece;
         console.log('i=',i,'randomPiece=',randomPiece,fileList[randomPiece]);
       }
@@ -289,132 +234,13 @@ function draw(){
     else if (playbackStarted==0){
       playButton.html("Play MIDI");
     }
-
-    //playButton.mousePressed(toggleMIDI);
-    viewButton = createButton('Preview Score');
-    viewButton.position(3*width/4, height/2-height/8);
-   // viewButton.mousePressed(showScore());
-    viewButton.mousePressed(() => showScore());
-  }
-}
-
-function intro1(){
-  for (let i=0, x=width/4; i<3; i++,x+=width/4){
-      randomPiece=randomNumList[i];
-      console.log(randomPiece,fileList[randomPiece]);
-      setParameters(randomPiece);
-     // console.log('timeSigNum='+timeSigNum);
-     // console.log('nLines='+nLines);
-      let y=height/2;
-      push();
-      drawViz(randomPiece, x, y, width/5, width/10, lineWeight, textHeight, colorPalette,strings,voice,percussion,woodwinds,keys,4,difficulty);
-      pop();
+    //print("spotifyID=",spotifyID);
+    if (!spotifyID){
+      openSpotifyButton.html('Not available on Spotify');
     }
-}
-
-function intro2(){
-  //x and y axes
-  let offset=width/8;
-  line(offset,height/2,width-offset,height/2);
-  line(width/2,offset,width/2,height-offset);
-
-  push();
-  textSize(textHeight);
-  textAlign(CENTER);
-  fill(0);
-  text('Minor',offset,height/2-5);
-  text('Major',width-offset,height/2-5);
-  text('Fast tempo',width/2,offset-5);
-  text('Slow tempo',width/2,height-offset+15);
-  fill(100);
-  textStyle(ITALIC);
-  text('Happy/upbeat',3*width/4,height/4);
-  text('Calm/peaceful',3*width/4,3*height/4);
-  text('Angry/intense',width/4,height/4);
-  text('Sad/melancholy',width/4,3*height/4);
-  pop();
-  [majorSlowColors,minorSlowColors,majorFastColors, minorFastColors]=defineColors();
-  noStroke();
-  circles(3*width/4-offset, 3*height/4-offset, majorSlowColors, width/8);
-  circles(3*width/4-offset, height/4+offset, majorFastColors, width/8);
-  circles(width/4+offset, 3*height/4-offset, minorSlowColors, width/8);
-  circles(width/4+offset, height/4+offset, minorFastColors, width/8);
-}
-
-function intro3(){
-  push();
-  colorPalette=majorFastColors;
-  strokeWeight(lineWeight);
-  fill(colorPalette[1]);
-  stroke(colorPalette[0]);
-
-  // time signature numerator=2 or 4: fill with squares
-  let midX=2*width/3;
-  let midY=height/4;
-  let outerHeight=width/8;
-  let innerHeight=outerHeight/2;
-  fill(colorPalette[1]);
-  rect(midX,midY,outerHeight);
-  fill(colorPalette[0]);
-  fillShapes(midX,midY,outerHeight,2,4,4);
-
-  //time signature numerator=3 or 6: fill with triangles
-  midX=width/3;
-  midY=height/4;
-  fill(colorPalette[1]);
-  rect(midX,midY,outerHeight);
-  fill(colorPalette[0]);
-  fillShapes(midX,midY,outerHeight,3,4,4);
-
-  //more notes per measure
-  midX=2*width/3;
-  midY=2*height/3;
-  fill(colorPalette[1]);
-  rect(midX,midY,outerHeight);
-  fill(colorPalette[0]);
-  fillShapes(midX,midY,outerHeight,2,9,4);
-
-  //fewer notes per measure
-  midX=width/3;
-  midY=2*height/3;
-  fill(colorPalette[1]);
-  rect(midX,midY,outerHeight);
-  fill(colorPalette[0]);
-  fillShapes(midX,midY,outerHeight,2,3,4);
-  pop();
-}
-
-function fillShapes(midX,midY,outerHeight,timeSigNum,nLines,shapeSize){
-  lineSpacing=outerHeight/nLines;
-    for (i=1; i<=nLines; i++){
-     //h=i*lineSpacing;
-     x=round(midX-outerHeight/2+i*lineSpacing-lineSpacing/2);
-
-     if (timeSigNum==2 | timeSigNum==4){
-       for (j=1; j<=nLines; j++){
-         y=round(midY-outerHeight/2+j*lineSpacing-lineSpacing/2);
-         rect(x,y,shapeSize,shapeSize);
-       }
-     }
-
-     else if (timeSigNum==3 | timeSigNum==6 | timeSigNum==5){
-       for (j=1; j<=nLines; j++){
-         y=round(midY-outerHeight/2+j*lineSpacing-lineSpacing/2);
-         triangle(x,y-shapeSize/2,
-                  x-shapeSize/sqrt(3), y + shapeSize/sqrt(3),
-                  x+shapeSize/sqrt(3), y + shapeSize/sqrt(3));
-        }
-     }
-   }
-}
-
-function circles(xCenter,yCenter,colors,d){
-  push();
-  fill(colors[0]);
-  arc(xCenter,yCenter,d,d,PI/2,3*PI/2);
-  fill(colors[1]);
-  arc(xCenter,yCenter,d,d,3*PI/2,PI/2);
-  pop();
+    else{
+      openSpotifyButton.html('Listen on Spotify');
+    }
 }
 
 function setParameters(randomPiece){
@@ -429,19 +255,72 @@ function setParameters(randomPiece){
   percussion=percussionList[randomPiece];
   woodwinds=woodwindsList[randomPiece];
   keys=keysList[randomPiece];
+  brass=brassList[randomPiece];
   difficulty=difficultyList[randomPiece];
 }
 
 function randomSort(){
-  //nPieces=60;
   randomNumList=[];
   for (let i=0; i < nscores - 1; i++){
     randomNumList.push(i);
   }
-  //console.log('randomNumList length=',randomNumList.length);
   randomNumList.sort(() => Math.random()-0.5);
 }
 
+function sortSimilar(){
+  similarityList=[];
+  for (let i=0; i<nscores-1; i++){
+    similarity=0; //for each piece, calculate a similarity score between that piece and the current selection
+    if (timeSigNumList[i]==timeSigNumList[newCenterPiece]){
+      similarity+=1;  //+1 for same time signature numerator
+    }
+
+    if (difficultyList[i]==difficultyList[newCenterPiece]){
+      similarity+=2;  //+2 for same difficulty score
+    }
+    else if (abs(difficultyList[i]-difficultyList[newCenterPiece])<=1){
+      similarity+=1;  //+1 for difficulty score within +/- 1
+    }
+
+    if (abs(nLinesList[i]-nLinesList[newCenterPiece])<=2){
+      similarity+=1;  //+1 for note density within +/- 2 notes/sec
+    }
+
+    if (stringsList[newCenterPiece]==stringsList[i] && voiceList[newCenterPiece]==voiceList[i] &&
+     keysList[newCenterPiece]==keysList[i] && woodwindsList[newCenterPiece]==woodwindsList[i] &&
+      brassList[newCenterPiece]==brassList[i]){
+         similarity+-2; //+2 for exact same instrument categories
+       }
+    else if ((stringsList[newCenterPiece]==1 & stringsList[i]==1) | (voiceList[newCenterPiece]==1 & voiceList[i]==1) |
+     (keysList[newCenterPiece]==1 & keysList[i]==1) | (woodwindsList[newCenterPiece]==1 & woodwindsList[i]==1) |
+     (brassList[newCenterPiece]==1 & brassList[i]==1)){
+         similarity+-1; //+1 for at least one match on instrument family
+       }
+
+    if ( (tempoList[i]>=100 && tempoList[newCenterPiece]>=100) | (tempoList[i]<100 && tempoList[newCenterPiece]<100)){
+      similarity+=1; //+1 if tempo is in same category
+    }
+    similarityList.push(similarity); //list of similarity scores for each piece
+  }
+  var simDistinct=[...new Set(similarityList)] //distinct values of similarity scores
+  simDistinct.sort(function(a,b){return b-a})  //sorted in descending order
+  function getAllIndexes(arr, val) {       //get list of all indexes in array corresponding to a specific value
+      var indexes = [], i;
+      for(i = 0; i < arr.length; i++)
+          if (arr[i] === val)
+              indexes.push(i);
+      return indexes;
+  }
+  sortedIndexList=[];  //get a list of indexes of pieces sorted by similarity score (best to worst match), initialize as index of current selection
+  for (i=0; i<simDistinct.length; i++){
+  		a=getAllIndexes(similarityList,simDistinct[i]);  //list of all indexes corresponding to a specific similarity score
+      sortedIndexList.push(a);
+  }
+  console.log('distinct values of similarity score',simDistinct);
+
+  randomNumList=sortedIndexList.flat(); //flatten multidimensional array
+    console.log('randomNumList',randomNumList);
+}
 
 function pieceData (featureData) {
   for (let i=0; i< nscores - 1; i++){
@@ -458,6 +337,7 @@ function pieceData (featureData) {
     woodwindsList.push(featureData[i].woodwinds);
     stringsList.push(featureData[i].strings);
     percussionList.push(featureData[i].percussion);
+    brassList.push(featureData[i].brass);
     fileList.push(featureData[i].filename);
     midiFileList.push(featureData[i].midiFileName);
     difficultyList.push(featureData[i].difficulty);
@@ -571,13 +451,8 @@ function setColors(i){
   return colorPalette;
 }
 
-// When the user clicks the mouse
+// When the user clicks the mouse on one of the smaller icons to change the current selection
 function mousePressed() {
-  if (frameCount<=4){
-    clear();
-    redraw();
-  }
-  else{
     for (let i=1; i<7; i++){
       if (i<=3){
           x=i/4*width;
@@ -595,23 +470,22 @@ function mousePressed() {
         redraw();
       }
     }
-  }
 }
 
-function question(){
-  textAlign(LEFT,CENTER);
-  textSize(textHeight-6);
-  text('Currently selected piece is the square in the center. Select a different square to change the current selection.',width/2,20,width*.8,textHeight*3);
-}
 
 function showScore(){
   scoreClicked+=1;   //number of times button clicked (to prevent multiple button instances)
   scoreOpen=1;
-  viewButton.hide();
+  //viewButton.hide();
 
-  //img=createImg(imageFile,"Image of score");
-  img.position(windowHeight,0);
-  img.size(windowWidth-windowHeight,(windowWidth-windowHeight)*img.height/img.width);
+  //img.position(windowHeight,0);
+  imgX=canvasW+downloadButton.width;
+  imgY=yoffset;
+  img.position(imgX,imgY);
+  //imgW = windowWidth-windowHeight;
+  imgW=(windowWidth-imgX)*.9;
+  imgH = imgW * img.height / img.width;
+  img.size(imgW, imgH);
   img.show();
 
   //if (typeof closeButton =="undefined"){
@@ -656,16 +530,20 @@ function toggleMIDI(){
 
 
 function openSpotify(){
-  if (spotifyOpen==0){
+  print('spotifyID=',spotifyID);
+  if (spotifyOpen==0 && spotifyID){
+      openSpotifyButton.html('Listen on Spotify');
     closeSpotifyButton.show();
     iframe=document.createElement('iframe');
     iframe.src=loc;
     iframe.allow="encrypted-media";
     iframe.width=myCanvas.width;
     iframe.height="80";
-    myContainer.appendChild(iframe);
+
+    //canvasContainer.appendChild(iframe);
+    spotifyContainer.appendChild(iframe);
+    //iframe.parent('exploreContainer');
     spotifyOpen=1;
-    //closeSpotifyButton.mousePressed(closeSpotify);
   }
 }
 
@@ -676,4 +554,19 @@ function closeSpotify(){
   closeSpotifyButton.hide();
   spotifyOpen=0;
   //redraw();
+}
+
+function selectEvent(){
+  let item = sel.value();
+  if (item == 'Show random pieces'){
+    toggleRandom=1;
+    console.log('sort order=random');
+  }
+  else {
+    toggleRandom=0;
+    console.log('sort order=similar');
+  }
+  randomize=1;
+  clear();
+  redraw();
 }
